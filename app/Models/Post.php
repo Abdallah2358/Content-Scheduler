9aab751582942ab876bbf9f69f5a2b14e8105646
عبدالله الحديدي <abdallah201@protonmail.com>
2025-05-25 16:07:40 +0300

    Refactor Post model to include fillable attributes, casts, and relationships for user and platforms

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,56 @@
 
 namespace App\Models;
 
+use App\Enums\PostStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'status',
+        'published_at',
+        'user_id',
+    ];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'status' => PostStatusEnum::class,
+        'published_at' => 'datetime',
+        'created_at' => 'datetime',
+    ];
+
+    /**
+     * Get the user that owns the post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the post published on platforms.
+     *
+     * @return BelongsToMany
+     */
+    public function platforms(): BelongsToMany
+    {
+        return $this->belongsToMany(Platform::class, 'post_platforms', 'post_id', 'platform_id');
+    }
 }
