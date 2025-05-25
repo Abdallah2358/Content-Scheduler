d2c3a207a0264a73896592c69d317821a7bcc7cc
عبدالله الحديدي <abdallah201@protonmail.com>
2025-05-25 19:28:45 +0300

    Refactor API routes to group post-related routes under auth middleware and remove unused user route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource(
+        'posts',
+        PostController::class
+    );
+    Route::get(
+        '/posts/{post}/platforms',
+        [PostController::class, 'platforms']
+    );
+});
