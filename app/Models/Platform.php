39147aa4eeb18267488d9396127914e196696b94
عبدالله الحديدي <abdallah201@protonmail.com>
2025-05-25 16:08:18 +0300

    Refactor Platform model to include fillable attributes and casts for type, created_at, and updated_at; add posts relationship

--- app/Models/Platform.php
+++ app/Models/Platform.php
@@ -2,11 +2,43 @@
 
 namespace App\Models;
 
+use App\Enums\PlatformTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Platform extends Model
 {
     /** @use HasFactory<\Database\Factories\PlatformFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'type'
+    ];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'type' => PlatformTypeEnum::class,
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Get the posts published on the platform.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class, 'post_platforms', 'platform_id', 'post_id');
+    }
 }
